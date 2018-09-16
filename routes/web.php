7675cc374593eed48274bc97d8f2731f26400a7e
cruelka <kes1993@ya.ru>
2018-09-16 20:06:49 +0400

    markup

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::get('page/{slug}', function($slug){
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+/*
 Route::get('/', function () {
     $posts = App\Post::where('category_id','1')->take(8)->get();
     $nash = App\Page::where('slug', '=', 'nash-magazin')->firstOrFail();
@@ -34,4 +35,8 @@ Route::get('/', function () {
     $footer= App\Page::where('slug', '=', 'zagolovok-v-futer')->firstOrFail();
 
     return view('main',compact('posts', 'nash','team','services','footer'));
+});
+*/
+Route::get('/', function () {
+return 'test';
 });
